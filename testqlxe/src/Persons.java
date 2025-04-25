bc4e3299185cb81c47c0d48cc1acac11a4169966
NguyenHieu-class <121001445+NguyenHieu-class@users.noreply.github.com>
2025-04-25 07:48:32 +0000

    Persons_class

---
+++ testqlxe/src/Persons.java
@@ -0,0 +1,17 @@
+import java.sql.Time;
+
+public class Persons {
+    int id;
+    String firstName;
+    String lastName;
+    Time birth;
+    String address;
+    Double money;
+
+    public Int toString(int id){
+
+        return id;
+    }
+
+    
+}
