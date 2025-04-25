df997d2bc95e73257a992e88b9b1f2f34ede4c31
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2025-04-25 07:47:34 +0000

    vehicle

---
+++ testqlxe/src/Vehicle.java
@@ -0,0 +1,17 @@
+public class Vehicle{
+    String TypeVehicle;
+    String NameVehicle;
+    int Amount;
+    String VehiclePlate;
+
+    public String DisplayName(String VehiclePlate){
+
+        return NameVehicle;
+
+    }
+     public int Avilability(String VehiclePlate){
+
+        return Amount;
+
+    }
+}
