1f5262e3c2a668a5adbd0fabfa5b0d1c17be7a1d
NguyenHieu-Class <22010103@st.phenikaa-uni.edu.vn>
2025-05-09 14:01:33 +0700

    Create User

---
+++ src/main/java/com/project/CarRental/model/User.java
@@ -0,0 +1,37 @@
+package com.project.CarRental.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class User {
+    private int idUser;
+    private String username;
+    private String password;
+    private String nameDisplay;
+    private String image;
+    private String phone;
+    private String email;
+
+    private Date createDate;
+    private Date updateDate;
+}
