69b26e90c5e513d6365df07f075ee47121a0b731
NguyenHieu-Class <22010103@st.phenikaa-uni.edu.vn>
2025-05-14 01:17:56 +0700

    Update class User

--- src/main/java/com/project/CarRental/model/User.java
+++ src/main/java/com/project/CarRental/model/User.java
@@ -1,27 +1,7 @@
 package com.project.CarRental.model;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 public class User {
     private int idUser;
@@ -31,7 +11,15 @@ public class User {
     private String image;
     private String phone;
     private String email;
+    private String address;
+    private String dateOfBirth;
+    private String drivingLicense;
+    private String ImgDrivingLicense;
+    private boolean sex;
 
     private Date createDate;
     private Date updateDate;
+
+    private int totalMoney;
+
 }
